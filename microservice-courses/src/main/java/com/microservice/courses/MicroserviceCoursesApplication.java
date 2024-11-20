ed037d136dbea8f25d3240bb9be95eca3dedc004
yannakode <yanann1997@gmail.com>
2024-11-20 15:31:24 -0300

    chore: initial structure and connections between microservices

---
+++ microservice-courses/src/main/java/com/microservice/courses/MicroserviceCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCoursesApplication.class, args);
+	}
+
+}
---
+++ microservice-students/src/test/java/com/microservice/students/MicroserviceStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
