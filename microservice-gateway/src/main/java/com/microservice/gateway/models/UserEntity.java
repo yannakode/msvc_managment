684248c1ec38f90ffb5c521008714a6c6d2dde9a
yannakode <yanann1997@gmail.com>
2024-11-25 14:47:37 -0300

    feat: implement user entity and user repository

---
+++ microservice-gateway/src/main/java/com/microservice/gateway/models/UserEntity.java
@@ -0,0 +1,23 @@
+package com.microservice.gateway.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
---
+++ microservice-gateway/src/main/java/com/microservice/gateway/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.gateway.repositories;
+
+import com.microservice.gateway.models.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+}
